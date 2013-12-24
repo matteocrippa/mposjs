d7d498178c5f1f0015f66500b03e9a311fff95a2
Matteo Crippa <matteo.crippa@gmail.com>
2013-12-24 14:42:58 +0100

    updated setup script

--- install-reqs.sh
+++ install-reqs.sh
@@ -7,5 +7,30 @@ echo 'Install requirements...'
 sudo apt-get install nodejs couchdb -y
 echo 'Installing Node.js modules...'
 npm install
-echo 'Installing Litecoind requirements'
-apt-get install build-essential libboost-all-dev libcurl4-openssl-dev libdb5.1-dev libdb5.1++-dev -y
+echo 'Install nodemon'
+sudo npm install nodemon -g
+echo 'Create Nginx host on port 80'
+sudo cat > /etc/nginx/sites-available/mposjs.conf << EOL
+upstream app_mposjs{
+				server 127.0.0.1:3000;
+}
+
+server {
+
+				listen 80;
+
+				access_log /var/log/mposjs_access.log;
+				error_log /var/log/mposjs_error.log;
+
+				location /{
+								proxy_set_header X-Real-IP $remote_addr;
+								proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+								proxy_set_header Host $http_host;
+								proxy_set_header X-NginX-Proxy true;
+
+								proxy_pass http://app_mposjs/;
+								proxy_redirect off;
+				}
+}
+EOL
+ln -s /etc/nginx/sites-available/mposjs.conf /etc/nginx/sites-enabled/mposjs.conf
